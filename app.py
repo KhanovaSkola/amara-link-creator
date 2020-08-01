9292a5b02c037cf6a0d76893ae3877153923cc73
Daniel Hollas <danekhollas@gmail.com>
2020-08-01 17:10:49 +0200

    Add Amara Public checkbox

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 # Run Flask server in debug mode
 # export FLASK_ENV=development
 # python -m flask run
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from kstools.api.amara_api import Amara
 app = Flask(__name__)
 
@@ -19,8 +19,12 @@ def get_amara_link(lang, youtube_id):
     """Polls Amara API for AmaraID for a given YouTubeID
     and constructs a link to the Amara subtitling editor"""
     # TODO: manage multiple YouTubeIDs per request
-    # TODO: Add option to generate public Amara links
-    # (need to validate the video exists, and if not, create it, and add a language)
+
+    public = request.args.get('public', 0, type=bool)
+    if public:
+        team = None
+    else:
+        team = AMARA_TEAM
 
     ytid_regex = r'^[a-zA-Z0-9_-]{11}$'
     if not re.fullmatch(ytid_regex, youtube_id):
@@ -28,22 +32,31 @@ def get_amara_link(lang, youtube_id):
 
     video_url = "https://youtube.com/watch?v=%s" % youtube_id
     amara = Amara()
-    amara_response = amara.check_video(video_url, AMARA_TEAM)
+    amara_response = amara.check_video(video_url, team)
+
+    # If video does not exist on Amara and we want public link
+    # we need to create it first
+    if public and amara_response['meta']['total_count'] == 0:
+        amara_response = amara.add_video(video_url, lang)
+        if not amara_response:
+            return {youtube_id: ("Could not add video to Amara", "")}
+
+        amara_id = amara_response['id']
+        editor_url = "%s/%s/subtitles/editor/%s/%s/" % \
+            (amara.AMARA_BASE_URL, lang, amara_id, lang)
+        return {youtube_id: (amara_id, editor_url)}
+
     for r in amara_response['objects']:
-        if r['team'] == AMARA_TEAM:
+        if not public and r['team'] == AMARA_TEAM:
             amara_id = r['id']
-
-            # This is all too complicated, let's just generate one link to
-            # video page
-            #editor_url = "%s/%s/subtitles/editor/%s/%s/?team=%s" % \
-            #    (amara.AMARA_BASE_URL, lang, amara_id, lang, AMARA_TEAM)
-            #assign_url = "%s/%s/videos/%s/collaborations/%s/join/subtitler/" % \
-            #    (amara.AMARA_BASE_URL, lang, amara_id, lang)
-            #review_url = "%s/%s/videos/%s/collaborations/%s/join/reviewer/" % \
-            #    (amara.AMARA_BASE_URL, lang, amara_id, lang)
-
             video_url = "%s/%s/videos/%s/%s/?team=%s" % \
-                (amara.AMARA_BASE_URL, lang, amara_id, lang, AMARA_TEAM)
+                (amara.AMARA_BASE_URL, lang, amara_id, lang, team)
             return {youtube_id: (amara_id, video_url)}
 
-    return {youtube_id: ("Could not find video on Amara", "", "")}
+        elif public and r['team'] is None:
+            amara_id = r['id']
+            editor_url = "%s/%s/subtitles/editor/%s/%s/" % \
+                (amara.AMARA_BASE_URL, lang, amara_id, lang)
+            return {youtube_id: (amara_id, editor_url)}
+
+    return {youtube_id: ("Could not find video on Amara", "")}
